ca8841f3121fee76edd800044a3e286c2e5bd068
Ashish Bhandari <ashish@proponenttech.com>
2024-07-25 09:05:54 +0530

    create router for page navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Button } from 'react-bootstrap';
+import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom' ;
 import Home from './pages/Home';
+import About from './pages/About';
+import Faq from './pages/Faq';
+import Services from './pages/Services';
+import Blog from './pages/Blog';
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/blog' element={<Blog/>} />
+      <Route path='/services' element={<Services/>} />
+      <Route path='/faq' element={<Faq/>} />
     </Routes>
     </BrowserRouter>
   )
